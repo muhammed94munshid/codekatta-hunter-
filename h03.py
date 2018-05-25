7bef25f8a48a07905eb82b673c57efe36c36ff0a
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-05-25 10:20:34 +0530

    Create h03.py

---
+++ h03.py
@@ -0,0 +1,23 @@
+n=int(input())
+l=[]
+for i in range (1,n+1):
+  a=int(input())
+  l.append(a)
+print (l)
+m=[]
+for idx,val in enumerate(l):
+  b=idx,val
+  m.append(b)
+print(m)
+for x in m:
+  
+
+  
+  
+
+  
+ 
+
+
+
+    
